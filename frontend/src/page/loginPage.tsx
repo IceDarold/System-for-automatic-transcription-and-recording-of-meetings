4c57b3ce5cbb0a20ea7dfed057d51de7458ca90c
valek2225 <valgalkin22@gmail.com>
2025-05-04 17:58:01 +0300

    last step before connect frontend and backend fast api

--- frontend/src/page/loginPage.tsx
+++ frontend/src/page/loginPage.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 export default function Login() {
   const [auth, setAuth] = React.useState("active-tabLogReg");
   const [reg, setReg] = React.useState("passive-tabLogReg");
-  const [step, setStep] = React.useState("first");
   const root = "http://127.0.0.1:8000";
-
-  const [loginData, setLoginData] = React.useState({
-    login: "",
-    password: "",
-  });
-
-  // Состояние для формы "Регистрация"
-  const [registrationData, setRegistrationData] = React.useState({
-    name: "",
-    region: "",
-    login: "",
-    password: "",
-  });
   const clickOnAuth = () => {
     setAuth("active-tabLogReg");
     setReg("passive-tabLogReg");
-
     const regForm = document.getElementById("regForm");
     const authForm = document.getElementById("authForm");
     const HeaderRegLogText = document.getElementById("HeaderRegLogText");
@@ -32,53 +17,6 @@ export default function Login() {
     }
     if (HeaderRegLogText) {
       HeaderRegLogText.textContent = "Вход в личный кабинет";
-    }
-  };
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    form: "login" | "registration"
-  ) => {
-    const { name, value } = e.target;
-
-    if (form === "login") {
-      setLoginData((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    } else {
-      setRegistrationData((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    }
-  };
-
-  const sendData = async (type: "login" | "registration") => {
-    const url = root + (type === "login" ? "/sendLogin" : "/sendRegistration");
-    console.log(url);
-    const body = JSON.stringify(
-      type === "login" ? loginData : registrationData
-    );
-
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body,
-      });
-
-      const result = await response.json();
-
-      if (response.ok) {
-        console.log("Успех:", result);
-      } else {
-        console.error("Ошибка:", result);
-      }
-    } catch (error) {
-      console.error("Ошибка сети:", error);
     }
   };
   const clickOnReg = () => {
@@ -117,90 +55,74 @@ export default function Login() {
                 </div>
               </div>
               <div className="FormRegLog">
-                {/* <form action={root + "/sendLogin"} method="post"> */}
-                <div className="authForm" id="authForm">
-                  <div className="input-container">
+                <form action={root + "/sendLogin"} method="post">
+                  <div className="authForm" id="authForm">
+                    <div className="input-container">
+                      <input
+                        className="inputForm"
+                        type="email"
+                        name="login"
+                        id="login-ath"
+                        required
+                      />
+                      <label htmlFor="login">Логин</label>
+                    </div>
+                    <div className="input-container">
+                      <input
+                        className="inputForm"
+                        type="password"
+                        name="password"
+                        id="password-ath"
+                        required
+                      />
+                      <label htmlFor="password">Пароль</label>
+                    </div>
                     <input
-                      className="inputForm"
-                      type="text"
-                      name="login"
-                      id="login-ath"
-                      value={loginData.login}
-                      onChange={(e) => handleChange(e, "login")}
-                      required
+                      className="sendFormButton"
+                      type="submit"
+                      value="Войти"
                     />
-                    <label htmlFor="login">Логин</label>
                   </div>
-                  <div className="input-container">
+                </form>
+                <form action={root + "/login/sendRegistration"} method="post">
+                  <div className="regForm" id="regForm">
+                    <div className="input-container">
+                      <input
+                        className="inputForm"
+                        type="text"
+                        name="name"
+                        id="name-reg"
+                        required
+                      />
+                      <label htmlFor="name">ФИО</label>
+                    </div>
+                    <div className="input-container">
+                      <input
+                        className="inputForm"
+                        type="email"
+                        name="login"
+                        id="login-reg"
+                        required
+                      />
+                      <label htmlFor="login">Логин</label>
+                    </div>
+                    <div className="input-container">
+                      <input
+                        className="inputForm"
+                        id="password-reg"
+                        type="password"
+                        name="password"
+                        required
+                      />
+                      <label htmlFor="password">Пароль</label>
+                    </div>
                     <input
-                      className="inputForm"
-                      type="password"
-                      name="password"
-                      id="password-ath"
-                      value={loginData.password}
-                      onChange={(e) => handleChange(e, "login")}
-                      required
+                      className="sendFormButton"
+                      type="submit"
+                      value="Войти"
                     />
-                    <label htmlFor="password">Пароль</label>
                   </div>
-                  <input
-                    className="sendFormButton"
-                    type="submit"
-                    value="Войти"
-                    onClick={() => {
-                      sendData("login");
-                    }}
-                  />
-                </div>
-                {/* </form> */}
-                {/* <form action={root + "/login/sendRegistration"} method="post"> */}
-                <div className="regForm" id="regForm">
-                  <div className="input-container">
-                    <input
-                      className="inputForm"
-                      type="text"
-                      name="name"
-                      id="name-reg"
-                      value={registrationData.name}
-                      onChange={(e) => handleChange(e, "registration")}
-                      required
-                    />
-                    <label htmlFor="name">ФИО</label>
-                  </div>
-                  <div className="input-container">
-                    <input
-                      className="inputForm"
-                      type="text"
-                      name="login"
-                      id="login-reg"
-                      value={registrationData.login}
-                      onChange={(e) => handleChange(e, "registration")}
-                      required
-                    />
-                    <label htmlFor="login">Логин</label>
-                  </div>
-                  <div className="input-container">
-                    <input
-                      className="inputForm"
-                      id="password-reg"
-                      type="password"
-                      name="password"
-                      value={registrationData.password}
-                      onChange={(e) => handleChange(e, "registration")}
-                      required
-                    />
-                    <label htmlFor="password">Пароль</label>
-                  </div>
-                  <input
-                    className="sendFormButton"
-                    type="submit"
-                    value="Войти"
-                    onClick={() => {
-                      sendData("registration");
-                    }}
-                  />
-                </div>
-                {/* </form> */}
+                </form>
               </div>
             </div>
           </div>
